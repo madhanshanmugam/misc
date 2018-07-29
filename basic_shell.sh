d8a6b8d5191ad950d141cb6f52fce058dc199523
madhan <madhan.s@freshdesk.com>
2018-07-30 00:11:24 +0530

    Added debugging steps

--- basic_shell.sh
+++ basic_shell.sh
@@ -1,9 +1,13 @@
+set -x # Debug mode (prints executed steps with variable values ) 
+set -e # Exit execution after any one steps fails (Non 0 return status)
+set -v # print verbose mode (both stm an resolved variables)
+
 read -p "enter a string " name # To read i/p from StdIn and store it in a name var 
 echo $name 
 echo $@ # print all args passed
 echo $0 # print first arg which is the function name 
-
-
+set +x # turn of debugging mode
+#ls "/abc"  uncomment this line to simulate non zero exit status
 function test(){
 	for arg in $@
 	do 
