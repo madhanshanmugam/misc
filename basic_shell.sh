df51b7b0f78fcd158d398f1756156d65488d4fad
madhan <madhan.s@freshdesk.com>
2018-12-21 18:32:36 +0530

    Added default argument  arg=#{arg:-"default"}

--- basic_shell.sh
+++ basic_shell.sh
@@ -3,6 +3,7 @@ set -e # Exit execution after any one steps fails (Non 0 return status)
 set -v # print verbose mode (both stm an resolved variables)
 
 read -p "enter a string " name # To read i/p from StdIn and store it in a name var 
+name=${name:-"default name"} # To set the default name if not passed
 echo $name 
 echo $@ # print all args passed
 echo $0 # print first arg which is the function name 
