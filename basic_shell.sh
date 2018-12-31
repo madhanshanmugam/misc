9ea15a21400d9345ad9205c268936023d5a2693a
madhan <madhan.s@freshdesk.com>
2018-12-31 14:32:36 +0530

    Added syntax for variable declaration

--- basic_shell.sh
+++ basic_shell.sh
@@ -1,6 +1,9 @@
 set -x # Debug mode (prints executed steps with variable values ) 
+set +x # turn of debugging mode
 set -e # Exit execution after any one steps fails (Non 0 return status)
 set -v # print verbose mode (both stm an resolved variables)
+
+declare -a name
 
 read -p "enter a string " name # To read i/p from StdIn and store it in a name var 
 name=${name:-"default name"} # To set the default name if not passed
@@ -9,8 +12,10 @@ echo $@ # print all args passed
 echo $0 # print first arg which is the function name 
 echo $? # return the status of last executed cmd 
 echo $$ # PID of the current process.
-set +x # turn of debugging mode
+
 #ls "/abc"  uncomment this line to simulate non zero exit status
+
+
 function test(){
 	for arg in $@
 	do 
