be932ff293ece23ca6a21c307eedc27d3bd914ce
madhan <madhanshines@gmail.com>
2019-03-28 13:46:13 +0530

    Added source and export explanation
    Added source and export explanation

--- basic_shell.sh
+++ basic_shell.sh
@@ -3,6 +3,13 @@ set +x # turn of debugging mode
 set -e # Exit execution after any one steps fails (Non 0 return status)
 set -v # print verbose mode (both stm an resolved variables)
 
+source ./Envfile # same as (. ./Envfile)
+#By default when we ran `./Envfile` instead of `source ./Envfile`, the commands in Envfile will be run by new parent process. 
+#so it cannot affect the parent shell's environment, but when we run `source ./Envfile`, commands in the Envfile will be run in current context
+# like running shell as parent process, so if have `exit` in the Envfile and running `source ./Envfile` will log you out but nothings happens for `./Envfile`
+# since its spwaning a new process and running it
+export passingVar="passedValue" # cmd to pass env varaible from parent context to newly created child processes 
+
 declare -a name
 
 read -p "enter a string " name # To read i/p from StdIn and store it in a name var 
